8390e19621c3adaa030598b1f895cbe6f0cd7842
charrrleee <charlcpy@gmail.com>
2023-02-04 18:01:53 -0500

    add registration profile 2

---
+++ app/src/main/java/com/team2/handiwork/fragments/RegistrationChooseRoleFragment.kt
@@ -0,0 +1,58 @@
+package com.team2.handiwork.fragments
+
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.team2.handiwork.R
+import com.team2.handiwork.databinding.FragmentRegistrationChooseRoleBinding
+import com.team2.handiwork.viewModel.RegistrationChooseRoleViewModel
+
+class RegistrationChooseRoleFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
+
+        val binding = FragmentRegistrationChooseRoleBinding.inflate(
+            inflater, container, false
+        )
+        val vm = RegistrationChooseRoleViewModel()
+        binding.vm = vm
+        binding.lifecycleOwner = this
+
+        // todo jump to next fragment
+        binding.btnCard1.setOnClickListener {
+            if ((it.background as ColorDrawable).color == R.color.blue_1200) {
+                it.setBackgroundResource(R.color.blue_200)
+                vm.isAgent.value = true
+            } else {
+                it.setBackgroundResource(R.color.blue_1200)
+                vm.isAgent.value = false
+            }
+        }
+
+        binding.btnCard2.setOnClickListener {
+            if ((it.background as ColorDrawable).color == R.color.blue_1200) {
+                it.setBackgroundResource(R.color.blue_200)
+                vm.isEmployer.value = true
+            } else {
+                it.setBackgroundResource(R.color.blue_1200)
+                vm.isEmployer.value = false
+            }
+        }
+
+        binding.btnDoBoth.setOnClickListener {
+            binding.ivCard1.setBackgroundResource(R.color.blue_1200)
+            binding.ivCard2.setBackgroundResource(R.color.blue_1200)
+            vm.isEmployer.value = true
+            vm.isAgent.value = true
+        }
+
+
+
+        return binding.root
+    }
+}
