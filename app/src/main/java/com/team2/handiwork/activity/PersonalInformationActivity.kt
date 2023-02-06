3c339cae035cbf75c0a94726cb1886225f83f278
charrrleee <charlcpy@gmail.com>
2023-02-05 22:43:48 -0500

    miss push

--- app/src/main/java/com/team2/handiwork/activity/PersonalInformationActivity.kt
+++ app/src/main/java/com/team2/handiwork/activity/PersonalInformationActivity.kt
@@ -4,13 +4,13 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.team2.handiwork.R
-import com.team2.handiwork.databinding.ActivityPersonalInformationBinding
+import com.team2.handiwork.databinding.ActivityRegistrationPersonalInformationBinding
 import com.team2.handiwork.viewModel.ActivityRegistrationPersonalInformationViewModel
 
 class PersonalInformationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = DataBindingUtil.setContentView<ActivityPersonalInformationBinding>(
+        val binding = DataBindingUtil.setContentView<ActivityRegistrationPersonalInformationBinding>(
             this,
             R.layout.activity_registration_personal_information
         )
