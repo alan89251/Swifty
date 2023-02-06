6e3dd131eed9fe0c207e1ccc3703c220b5afd08f
Steven Lai <wai30115@live.hk>
2023-02-05 22:44:28 -0500

    Update forgot password UI & function

---
+++ app/src/main/java/com/team2/handiwork/utilities/Event.kt
@@ -0,0 +1,17 @@
+package com.team2.handiwork.utilities
+
+open class Event<T>(private val content: T) {
+    var hasBeenHandled = false
+        private set
+
+    fun getContentIfNotHandled(): T? {
+        return if (hasBeenHandled) {
+            null
+        } else {
+            hasBeenHandled = true
+            content
+        }
+    }
+
+    fun peekContent(): T = content
+}
