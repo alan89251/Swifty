52b3b431dbe47dfef8455decc204da470a36268a
Steven Lai <wai30115@live.hk>
2023-02-10 03:26:23 -0500

    Add navigation host fragment in home activity & set up basic navigation graph

--- app/src/main/java/com/team2/handiwork/HomeActivity.kt
+++ app/src/main/java/com/team2/handiwork/HomeActivity.kt
@@ -2,10 +2,40 @@ package com.team2.handiwork
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.*
+import com.team2.handiwork.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+    lateinit var binding: ActivityHomeBinding
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var navController: NavController
+    private lateinit var listener: NavController.OnDestinationChangedListener
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_home)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+        appBarConfiguration = AppBarConfiguration(navController.graph, binding.drawerLayout)
+
+        binding.apply {
+
+            navView.setupWithNavController(navController)
+            setupActionBarWithNavController(navController, appBarConfiguration)
+
+        }
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(binding.navHostFragment.id)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
 }
