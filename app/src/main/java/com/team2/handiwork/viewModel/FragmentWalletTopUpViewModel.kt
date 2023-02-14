12342541a7845eda3e3c5f2adb76e5261979a431
charrrleee <charlcpy@gmail.com>
2023-02-14 00:36:11 -0500

    add topupsuccess and minor fix

--- app/src/main/java/com/team2/handiwork/viewModel/FragmentWalletTopUpViewModel.kt
+++ app/src/main/java/com/team2/handiwork/viewModel/FragmentWalletTopUpViewModel.kt
@@ -1,5 +1,6 @@
 package com.team2.handiwork.viewModel
 
+import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
@@ -8,4 +9,24 @@ class FragmentWalletTopUpViewModel : ViewModel() {
     var cvv = MutableLiveData<String>("")
     var expiredDate = MutableLiveData<String>("")
     var topUpAmount = MutableLiveData<Int>(50)
+
+    var topUpBtnEnabled: MediatorLiveData<Boolean> = MediatorLiveData<Boolean>()
+
+    init {
+        topUpBtnEnabled.addSource(creditCardNo) { checkBtnEnable() }
+        topUpBtnEnabled.addSource(cvv) { checkBtnEnable() }
+        topUpBtnEnabled.addSource(expiredDate) { checkBtnEnable() }
+    }
+
+    private fun checkBtnEnable() {
+        if (creditCardNo.value!!.length < 16
+            || cvv.value!!.length < 3
+            || expiredDate.value!!.length < 4
+        ) {
+            topUpBtnEnabled.value = false
+            return
+        }
+        topUpBtnEnabled.value = true
+    }
+
 }
