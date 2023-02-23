20888c905aea3a95cbf7b2f5752ac38e0c519ebd
Steven Lai <wai30115@live.hk>
2023-02-22 19:29:37 -0500

    Update switch nav host fragment dynamically

--- app/src/main/java/com/team2/handiwork/HomeActivity.kt
+++ app/src/main/java/com/team2/handiwork/HomeActivity.kt
@@ -5,6 +5,7 @@ import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.MenuInflater
 import android.view.MenuItem
 import android.widget.TextView
 import androidx.activity.viewModels
@@ -32,16 +33,17 @@ class HomeActivity : AppCompatActivity() {
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var navController: NavController
     private val viewModel by viewModels<ActivityHomeViewModel>()
+    private var isEmployer = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val pref = PreferenceManager.getDefaultSharedPreferences(this)
         val currentTheme = pref.getInt(AppConst.CURRENT_THEME, 0)
+        isEmployer = currentTheme == 1
         Utility.onActivityCreateSetTheme(this)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_home)
         val headerView = binding.navView.getHeaderView(0)
 
 
-        // set the header & navigation view UI information
         val userEmail = pref.getString(AppConst.EMAIL, "")!!
         viewModel.getUserByEmail(userEmail).subscribe { user ->
             val emailTextView = headerView.findViewById<TextView>(R.id.header_email)
@@ -54,39 +56,22 @@ class HomeActivity : AppCompatActivity() {
 
         viewModel.getUserMission(userEmail)
 
-        //set up navigation drawer & action bar
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        navController = navHostFragment.navController
-        appBarConfiguration = AppBarConfiguration(setOf(R.id.homeFragment,R.id.walletBalanceFragment,R.id.myMissionsFragment), binding.drawerLayout)
+        setHomeScreen()
 
-        binding.apply {
-            navView.setupWithNavController(navController)
-            setupActionBarWithNavController(navController, appBarConfiguration)
+        binding.switchButton.text = if (isEmployer) {
+            "Switch To Agent Portal"
+        } else {
+            "Switch To Employer Portal"
+        }
+        binding.navView.menu.findItem(R.id.portal_name).title = if (isEmployer) {
+            "Employer Portal"
+        } else {
+            "Agent Portal"
         }
 
         //switch theme button
         binding.switchButton.setOnClickListener {
-            val pref = PreferenceManager.getDefaultSharedPreferences(this)
-            val editor: SharedPreferences.Editor = pref.edit()
-            if (currentTheme == 0) {
-                Utility.changeToTheme(this, Utility.THEME_EMPLOYER)
-                editor.putInt(AppConst.CURRENT_THEME, 1)
-            } else {
-                Utility.changeToTheme(this, Utility.THEME_AGENT)
-                editor.putInt(AppConst.CURRENT_THEME, 0)
-            }
-            editor.apply()
-        }
-
-        binding.switchButton.text = if (currentTheme == 0) {
-            "Switch To Employer Portal"
-        } else {
-            "Switch To Agent Portal"
-        }
-        binding.navView.menu.findItem(R.id.portal_name).title = if (currentTheme == 0) {
-            "Agent Portal"
-        } else {
-            "Employer Portal"
+            switchTheme()
         }
 
         // logout button
@@ -96,6 +81,50 @@ class HomeActivity : AppCompatActivity() {
             intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
             startActivity(intent)
             finish()
+        }
+    }
+
+
+    private fun switchTheme() {
+        val pref = PreferenceManager.getDefaultSharedPreferences(this)
+        val editor: SharedPreferences.Editor = pref.edit()
+        if (!isEmployer) {
+            editor.putInt(AppConst.CURRENT_THEME, 1)
+            editor.apply()
+            Utility.changeToTheme(this, Utility.THEME_EMPLOYER)
+        } else {
+            editor.putInt(AppConst.CURRENT_THEME, 0)
+            editor.apply()
+            Utility.changeToTheme(this, Utility.THEME_AGENT)
+        }
+    }
+
+    // switch the nav host fragment start destination & navigation view menu
+    private fun setHomeScreen() {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val inflater = navHostFragment.navController.navInflater
+        val graph = inflater.inflate(R.navigation.nav_graph)
+        binding.navView.menu.clear()
+        val menuInflater = MenuInflater(this)
+        if (isEmployer) {
+            graph.setStartDestination(R.id.homeFragment)
+            menuInflater.inflate(R.menu.nav_menu, binding.navView.menu)
+        } else {
+            graph.setStartDestination(R.id.agentHomeFragment)
+            menuInflater.inflate(R.menu.agent_nav_menu, binding.navView.menu)
+        }
+        val navController = navHostFragment.navController
+        navController.setGraph(graph, intent.extras)
+
+        val homeID = if (isEmployer) R.id.homeFragment else R.id.agentHomeFragment
+
+        appBarConfiguration = AppBarConfiguration(
+            setOf(homeID, R.id.walletBalanceFragment, R.id.myMissionsFragment),
+            binding.drawerLayout
+        )
+        binding.apply {
+            navView.setupWithNavController(navController)
+            setupActionBarWithNavController(navController, appBarConfiguration)
         }
     }
 
