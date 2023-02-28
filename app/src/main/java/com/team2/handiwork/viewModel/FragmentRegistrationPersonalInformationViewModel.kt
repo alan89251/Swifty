70f952f8514ad1c24dce542620b7de066ff55f16
charrrleee <charlcpy@gmail.com>
2023-02-28 18:06:47 -0500

    fix data existing when popback is click

--- app/src/main/java/com/team2/handiwork/viewModel/FragmentRegistrationPersonalInformationViewModel.kt
+++ app/src/main/java/com/team2/handiwork/viewModel/FragmentRegistrationPersonalInformationViewModel.kt
@@ -3,12 +3,18 @@ package com.team2.handiwork.viewModel
 import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.team2.handiwork.models.User
 
 class FragmentRegistrationPersonalInformationViewModel : ViewModel() {
-    var firstName = MutableLiveData("")
-    var lastName = MutableLiveData("")
-    var phoneNumber = MutableLiveData("")
+    // temp form
+    var form = MutableLiveData<User>(User())
+
+    var firstName = MutableLiveData(form.value!!.firstName)
+    var lastName = MutableLiveData(form.value!!.lastName)
+    var phoneNumber = MutableLiveData(form.value!!.phoneNumber)
     var verifyMsg = MutableLiveData("")
+    var email = MutableLiveData(form.value!!.email)
+
 
     var nextBtnEnabled: MediatorLiveData<Boolean> = MediatorLiveData<Boolean>()
 
